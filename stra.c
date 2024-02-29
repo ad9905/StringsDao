934902b6d1c38281f69fbe2beca863d787804bfe
ad9905 <158365363+ad9905@users.noreply.github.com>
2024-02-28 20:23:04 -0500

    Add files via upload
    added main method, first attempt at getting the functions to run.

--- stra.c
+++ stra.c
@@ -15,18 +15,18 @@ size_t Str_getLength(const char pcSrc[])
     return uLength;
 }
 
-size_t Str_copy(char copy[], const char pcSrc[])
+char *Str_copy(char pcDest[], const char pcSrc[])
 {
     size_t i = 0;
-    assert(copy != NULL && pcSrc != NULL);
-    while (copy[i] = pcSrc[i] != '\0')
+    assert(pcDest != NULL && pcSrc != NULL);
+    while (pcDest[i] = pcSrc[i] != '\0')
         i++;
     /* \0 because NULL represents a null pointer, and \0 is an empty
     string literal */
-    return i;
+    return pcDest;
 }
 
-size_t Str_concat(char dest[], const char pcSrc[])
+char *Str_concat(char dest[], const char pcSrc[])
 {
     size_t destLength = Str_getLength(dest);
     size_t i = 0;
@@ -37,23 +37,37 @@ size_t Str_concat(char dest[], const char pcSrc[])
         i++;
     }
     dest[destLength + i] = '\0';
-    return destLength + i;
-    /* Return the length of the concatenated string */
+    return dest;
+    /* Return the destination string */
 }
 
+/* should be returning integers that denote if strings are equal,
+less than, or greater than. */
 size_t Str_compare(const char str1[], const char str2[])
 {
     size_t i = 0;
     assert(str1 != NULL && str2 != NULL);
+    /* should return how the strings differ:
+    -1 if string 1 is less than string 2
+    1 if string 1 is greater than string 2 */
     while (str1[i] == str2[i])
     {
-        if (str1[i] == '\0')
-            return 0;
+        if (str1[i] > str2[i])
+        {
+            return 1;
+        }
+        else if (str1[i] < str2[i])
+        {
+            return -1;
+        }
+
         /* return 0 means strings are equal */
+
+        else if (str1[i] == '\0')
+            return 0;
         i++;
     }
-    return i;
-    /* i is index where the strings differ */
+    return 0;
 }
 
 size_t Str_search(const char haystack[], const char needle[])
@@ -83,8 +97,42 @@ size_t Str_search(const char haystack[], const char needle[])
         }
         if (j == needle_len)
         {
-            return i; /* The index of needle */
+            /* The index of needle
+             (its starting position)*/
+            return i;
         }
     }
     return -1; /* Needle not found */
+}
+
+int main()
+{
+    /* Test strings */
+    char str1[] = "Hello";
+    char str2[] = "Hello";
+    char str3[] = "Hello, world!";
+    char needle[] = "world";
+    char dest[50] = "Concatenate ";
+
+    /* Test my Str_getLength vs library's strlen() */
+    printf("Str_getLength() result: %ld\n", Str_getLength(str1));
+    printf("strlen() result: %ld\n", strlen(str1));
+
+    /* Test my Str_compare() vs library's strcmp() */
+    printf("Str_compare() result: %d\n", Str_compare(str1, str2));
+    printf("strcmp() result: %d\n", strcmp(str1, str2));
+
+    /* Test my Str_search() vs library's strstr() */
+    printf("Str_search() result: %d\n", Str_search(str1, str2));
+    printf("strstr() result: %d\n", strstr(str1, str2));
+
+    /* Test my Str_concat vs library's str*/
+    printf("Str_concat() result: %d\n", Str_concat(dest, str2));
+    printf("strcat() result: %d\n", strcat(dest, str2));
+
+    /* Test copy */
+    printf("Str_copy() result: %d\n", Str_copy(str1, str2));
+    printf("strcpy() result: %d\n", strcpy(str1, str2));
+
+    return 0;
 }
