f07959b945ef8a4eec9a622b2b8ce1abe1e5d2ad
ad9905 <158365363+ad9905@users.noreply.github.com>
2024-02-28 20:45:39 -0500

    Add files via upload

--- stra.c
+++ stra.c
@@ -75,13 +75,11 @@ int Str_compare(const char str1[], const char str2[])
 
 char *Str_search(const char haystack[], const char needle[])
 {
-    assert(haystack != NULL && needle != NULL);
-
     int i;
     int j;
     int haystack_len = 0;
     int needle_len = 0;
-
+    assert(haystack != NULL && needle != NULL);
     haystack_len = Str_getLength(haystack);
     needle_len = Str_getLength(needle);
 
