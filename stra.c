9cf9e2765da3455a086ad12c5696b25fbd1c15be
ad9905 <158365363+ad9905@users.noreply.github.com>
2024-02-28 21:35:26 -0500

    Add files via upload

--- stra.c
+++ stra.c
@@ -68,7 +68,15 @@ int Str_compare(const char str1[], const char str2[])
         }
         i++;
     }
-    return 0;
+    if (str1[i] == '\0' && str2[i] == '\0')
+    {
+        return 0;
+    }
+    else if (str1[i] == '\0')
+    {
+        return -1;
+    }
+    return 1;
 }
 
 char *Str_search(const char haystack[], const char needle[])
